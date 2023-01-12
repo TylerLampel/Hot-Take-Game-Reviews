51557ded6deadb08a467155d681d49c891a5c585
Tyler Lampel <lampel13@gmail.com>
2023-01-11 21:47:21 -0500

    Adding NavBar component

---
+++ client/src/NavBar.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { UserContext } from "./context/user";
+import { NavLink, useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const { user, logout, loggedIn } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function logoutUser() {
+    fetch("/logout", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    }).then(() => {
+      logout();
+      navigate("/");
+    });
+  }
+
+  if (loggedIn) {
+    return (
+      <div>
+        <h3>Hello {user.username}</h3>
+        <button onClick={logoutUser}>Logout</button>
+        <hr />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NavLink to="/login">
+          <button>Login</button>
+        </NavLink>
+        <NavLink to="/signup">
+          <button>Signup</button>
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
