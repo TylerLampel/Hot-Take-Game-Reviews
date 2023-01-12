3e2a29cb1c34163366ef195ec7a3b0f233bc3b52
Tyler Lampel <lampel13@gmail.com>
2023-01-11 21:45:17 -0500

    Adding Home component

---
+++ client/src/Home.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { UserContext } from "./context/user";
+
+function Home() {
+  const { user, loggedIn } = useContext(UserContext);
+
+  if (loggedIn) {
+    return (
+      <div>
+        <h3>{user.username}'s Home Page</h3>
+      </div>
+    );
+  } else {
+    return <h3>Please Login or Signup</h3>;
+  }
+}
+
+export default Home;
