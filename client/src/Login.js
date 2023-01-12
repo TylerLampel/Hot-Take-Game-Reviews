97cf89c974444c43d22a43108527718441d2c24e
Tyler Lampel <lampel13@gmail.com>
2023-01-11 21:46:48 -0500

    Adding Login Component

---
+++ client/src/Login.js
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from "react";
+import { UserContext } from "./context/user";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { login } = useContext(UserContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }).then((res) => res.json());
+  }
+  return;
+}
+
+export default Login;
