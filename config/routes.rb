9a580b7d1f85f8fe1aaafc6d8dbbaf949df2ec69
Tyler Lampel <lampel13@gmail.com>
2023-01-11 21:19:03 -0500

    adding routes for login, logout, signup, auto login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :users
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessons#destroy"
+
+  post "/signup", to: "users#create"
+  post "/me", to: "users#show"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
